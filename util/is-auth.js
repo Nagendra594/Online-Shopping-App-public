e698ae318c56472a600d30f9a823ba504d6d950f
nagendra594 <nagendra594@outlook.com>
2024-03-08 13:06:56 +0530

    Prepared For Development

--- util/is-auth.js
+++ util/is-auth.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
   if (!req.session.isLoggedInAsAdmin && !req.session.isLoggedInAsUser) {
+    console.log("Not Authenticated");
     return res.redirect("/login");
   }
+  console.log("Authenticated");
   next();
 };
